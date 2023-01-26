773b1893a13da2ae419ca312066c089ec536cca2
Andreas Vögele <andreas@andreasvoegele.com>
2023-01-26 09:26:20 +0100

    Add shp_point_is_collinear()

--- shp-point.h
+++ shp-point.h
@@ -26,4 +26,38 @@ typedef struct shp_point_t {
     double y; /**< The vertical position */
 } shp_point_t;
 
+/**
+ * Check whether three points lie on a straight line
+ *
+ * Determines whether three points lie on a straight line.
+ *
+ * @memberof shp_point_t
+ * @param point the first point.
+ * @param a the second point.
+ * @param b the third point.
+ * @param epsilon an error factor, for example 1e-9.
+ * @retval 1 if the points lie on a straight line.
+ * @retval 0 if the points do not lie on a straight line.
+ */
+extern int shp_point_is_collinear(const shp_point_t *point,
+                                  const shp_point_t *a, const shp_point_t *b,
+                                  double epsilon);
+
+/**
+ * Check whether a point lies between two points on a straight line
+ *
+ * Determines whether a point lies between two points on a straight line.
+ *
+ * @memberof shp_point_t
+ * @param point a point.
+ * @param a the start point.
+ * @param b the end point.
+ * @param epsilon an error factor, for example 1e-9.
+ * @retval 1 if the point lies between @c a and @c b (inclusive).
+ * @retval 0 if the point does not lie between @c a and @c b.
+ */
+extern int shp_point_is_between(const shp_point_t *point,
+                                const shp_point_t *a, const shp_point_t *b,
+                                double epsilon);
+
 #endif
--- tests/test_point.c
+++ tests/test_point.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#define EPSILON 1e-9
+
 int tests_planned = 0;
 int tests_run = 0;
 int tests_failed = 0;
@@ -63,18 +65,194 @@ test_shp(void)
     point = &shp_record->shape.point;
     switch (record_number) {
     case 0:
-        ok(test_freiburg, "Freiburg");
+        ok(test_freiburg, "location is Freiburg");
         break;
     case 1:
-        ok(test_karlsruhe, "Karlsruhe");
+        ok(test_karlsruhe, "location is Karlsruhe");
         break;
     case 2:
-        ok(test_mannheim, "Mannheim");
+        ok(test_mannheim, "location is Mannheim");
         break;
     case 3:
-        ok(test_stuttgart, "Stuttgart");
+        ok(test_stuttgart, "location is Stuttgart");
         break;
     }
+}
+
+static int
+test_decimals(void)
+{
+    shp_point_t p[3] = {{0, 0.3}, {1, 0.6}, {2, 0.9}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], EPSILON) != 0;
+}
+
+static int
+test_decimals_no_epsilon(void)
+{
+    shp_point_t p[3] = {{0, 0.3}, {1, 0.6}, {2, 0.9}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_rationals(void)
+{
+    shp_point_t p[3] = {{0, 1.0 / 3.0}, {1, 2.0 / 3.0}, {2, 1}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], EPSILON) != 0;
+}
+
+static int
+test_rationals_no_epsilon(void)
+{
+    shp_point_t p[3] = {{0, 1.0 / 3.0}, {1, 2.0 / 3.0}, {2, 1}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_integers(void)
+{
+    shp_point_t p[3] = {{0, 0}, {1, 1}, {2, 2}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], EPSILON) != 0;
+}
+
+static int
+test_integers_no_epsilon(void)
+{
+    shp_point_t p[3] = {{0, 0}, {1, 1}, {2, 2}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_x_is_x1(void)
+{
+    shp_point_t p[3] = {{-1, 0}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_x_is_x2(void)
+{
+    shp_point_t p[3] = {{1, 0}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_x_between_x1_and_x2(void)
+{
+    shp_point_t p[3] = {{0, 0}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_x_between_x2_and_x1(void)
+{
+    shp_point_t p[3] = {{0, 0}, {1, 0}, {-1, 0}};
+    return shp_point_is_between(&p[0], &p[2], &p[1], 0) != 0;
+}
+
+static int
+test_x_is_left_of_x1(void)
+{
+    shp_point_t p[3] = {{-2, 0}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_x_is_right_of_x2(void)
+{
+    shp_point_t p[3] = {{3, 0}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_x_is_below_x1(void)
+{
+    shp_point_t p[3] = {{-1, -1}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_x_is_above_x2(void)
+{
+    shp_point_t p[3] = {{1, 1}, {-1, 0}, {1, 0}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_y_is_y1(void)
+{
+    shp_point_t p[3] = {{0, -1}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_y_is_y2(void)
+{
+    shp_point_t p[3] = {{0, 1}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_y_between_y1_and_y2(void)
+{
+    shp_point_t p[3] = {{0, 0}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_y_between_y2_and_y1(void)
+{
+    shp_point_t p[3] = {{0, 0}, {0, 1}, {0, -1}};
+    return shp_point_is_between(&p[0], &p[2], &p[1], 0) != 0;
+}
+
+static int
+test_y_is_left_of_y1(void)
+{
+    shp_point_t p[3] = {{-1, -1}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_y_is_right_of_y2(void)
+{
+    shp_point_t p[3] = {{1, 1}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_y_is_below_y1(void)
+{
+    shp_point_t p[3] = {{0, -2}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_y_is_above_y2(void)
+{
+    shp_point_t p[3] = {{0, 2}, {0, -1}, {0, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
+}
+
+static int
+test_is_between_1(void)
+{
+    shp_point_t p[3] = {{0, 0}, {-1, -1}, {1, 1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_is_between_2(void)
+{
+    shp_point_t p[3] = {{0, 0}, {-1, 1}, {1, -1}};
+    return shp_point_is_between(&p[0], &p[1], &p[2], 0) != 0;
+}
+
+static int
+test_is_not_between(void)
+{
+    shp_point_t p[3] = {{-1, -1}, {0, 0}, {1, 1}};
+    return shp_point_is_collinear(&p[0], &p[1], &p[2], 0) != 0 &&
+           shp_point_is_between(&p[0], &p[1], &p[2], 0) == 0;
 }
 
 int
@@ -83,7 +261,7 @@ main(int argc, char *argv[])
     const char *datadir = getenv("datadir");
     FILE *shp_fp;
 
-    plan(9);
+    plan(34);
 
     if (datadir == NULL) {
         fprintf(stderr,
@@ -110,7 +288,6 @@ main(int argc, char *argv[])
         ok(test_header_shape_type, "header shape type is point");
         record_number = 0;
         while (shp_read_record(&shp_fh, &shp_record) > 0) {
-        fprintf(stderr, "# bar\n");
             test_shp();
             free(shp_record);
             ++record_number;
@@ -119,6 +296,35 @@ main(int argc, char *argv[])
     }
 
     fclose(shp_fp);
+
+    ok(test_decimals, "collinear decimals with epsilon");
+    ok(test_decimals_no_epsilon, "inexact decimals without epsilon");
+    ok(test_rationals, "collinear rationals with epsilon");
+    ok(test_rationals_no_epsilon, "inexact rationals without epsilon");
+    ok(test_integers, "collinear integers with epsilon");
+    ok(test_integers_no_epsilon, "collinear integers without epsilon");
+
+    ok(test_x_is_x1, "x is x1");
+    ok(test_x_is_x2, "x is x2");
+    ok(test_x_between_x1_and_x2, "x is between x1 and x2");
+    ok(test_x_between_x2_and_x1, "x is between x2 and x1");
+    ok(test_x_is_left_of_x1, "x is left of x1");
+    ok(test_x_is_right_of_x2, "x is right of x2");
+    ok(test_x_is_below_x1, "x is below x1");
+    ok(test_x_is_above_x2, "x is above x2");
+
+    ok(test_y_is_y1, "y is y1");
+    ok(test_y_is_y2, "y is y2");
+    ok(test_y_between_y1_and_y2, "y is between y1 and y2");
+    ok(test_y_between_y2_and_y1, "y is between y2 and y1");
+    ok(test_y_is_left_of_y1, "y is left of y1");
+    ok(test_y_is_right_of_y2, "y is right of y2");
+    ok(test_y_is_below_y1, "y is below y1");
+    ok(test_y_is_above_y2, "y is above y2");
+
+    ok(test_is_between_1, "point is between two points");
+    ok(test_is_between_2, "point is between two other points");
+    ok(test_is_not_between, "collinear point is not between two points");
 
     done_testing();
 }
